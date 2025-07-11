4decc3461c34d9f09973f5aab03afdb3ffa0e7d5
D. Port Me <dport@hawaii.edu>
2025-07-10 14:49:16 -1000

    fixed CSV parsing problem of descartes-courses.csv

---
+++ module-info.js
@@ -0,0 +1,9 @@
+DES 101 = {
+modules: [
+  { course: "DES 101", title: "UHM DESCARTES Welcome", moduleUrl: "/descartes-modules/modules/descartes-welcome", sort_order: 21, description: "CHANGE ME\n" },
+  { course: "DES 101", title: "Exploratory Time Series Analysis in R", moduleUrl: "/descartes-modules/modules/time-series-analysis", sort_order: 80, description: "A workshop covering modeling and analysis of climate-change time-series data using R.\n" }
+],
+ prerequisites: [
+  { moduleUrl: "/descartes-modules/modules/time-series-analysis", prerequisiteUrl: "/descartes-modules/modules/descartes-welcome" }
+]
+};
